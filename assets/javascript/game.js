6cda715e7db4e7f15e57954df4f3b2f40239cbfb
Michael Williams <mwilliams2964@gmail.com>
2019-09-02 00:14:45 -0700

    added alphabet to make sure other keys did not count as guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
-let wordArr = ["skateboard", "airplane", "yacht", "computer", "travel", "fun"];
+let wordArr = ["skateboard", "airplane", "yacht", "computer", "travel", "fun", "chairs"];
+let alphabetStr = "qwertyuiopasdfghjklzxcvbnm";
+let alphabetArr = alphabetStr.split("");
 let displayWord = document.getElementById("current-word");
 let displayLetter = document.getElementById("already-guessed");
 let underscores = "";
@@ -15,6 +17,81 @@ winsDisplay.textContent = "Wins: " + wins;
 // document.getElementById("wins").textContent = "Wins: " + wins;
 let guessLeft = 13;
 
+// Math.floor() rounds to the nearest integer
+// Math.random() chooses a random number between 0 and 1. If I want to,
+// specify a range, then I multiply Math.random() by a range I want to
+// set- in this case the range is the length of my word
+console.log(currentWord);
+
+// this function makes letter blanks
+for (let i = 0; i < currentWord.length; i++) {
+    underscores = underscores + "_ ";
+    underscoresArr = underscores.split(" ");
+    console.log(underscoresArr);
+    displayWord.textContent = underscores;
+}
+
+// This function handles key events and counts guesses in the "Letters already guessed"
+document.onkeyup = function (event) {
+    let userGuess = event.key;
+    let guessedLetter = userGuess;
+
+    if (alphabetArr.indexOf(guessedLetter) >= 0) {
+
+
+
+        if (guessedLetters.indexOf(guessedLetter) >= 0) {
+            // this makes sure the user can't guess the same letter more than once
+            alert("You've already guessed '" + userGuess + "'. Try a different letter.")
+        }
+        else {
+            // This logic appends every guess to Letters Already Guessed
+            guessedLetters.push(guessedLetter);
+            console.log(guessedLetters);
+            console.log(typeof (guessedLetters));
+            displayLetter.textContent = guessedLetters;
+            guessLeft--;
+        }
+
+        // the line below displays each of the users guesses
+        displayLetter.textContent = guessedLetters;
+
+        console.log("guessedLetter: " + guessedLetter);
+        console.log("guessedLetters" + " " + guessedLetters);
+
+        // below updates the guesses left to the browser
+        document.getElementById("remaining-guesses").innerHTML = guessLeft;
+
+        if (guessLeft > 0) {
+
+            for (let i = 0; i < letterArr.length; i++) {
+
+                if (guessedLetter === letterArr[i]) {
+                    console.log(guessedLetter);
+                    underscoresArr.splice(i, 1, guessedLetter);
+
+                    console.log(underscoresArr);
+                    displayWord.textContent = underscoresArr.join(" ");
+                }
+            }
+        } else {
+
+            document.getElementById("remaining-guesses").innerHTML = guessLeft;
+            wins++;
+            winsDisplay;
+            alert("GAME OVER!");
+            reset();
+        }
+        if (underscoresArr.includes("_") === false) {
+            wins++;
+            winsDisplay.textContent = "Wins: " + wins;
+            setTimeout(function () {
+                alert("You Won!");
+            })
+            reset();
+        }
+    }
+}
 
 function reset() {
     guessLeft = 13;
@@ -25,104 +102,17 @@ function reset() {
     console.log(currentWord)
     letterArr = currentWord.split("");
     for (let i = 0; i < currentWord.length; i++) {
-        underscores = underscores + "_";
+        underscores = underscores + "_ ";
         console.log(underscores)
-        underscoresArr = underscores.split("");
+        underscoresArr = underscores.split(" ");
         console.log(underscoresArr);
         displayWord.textContent = underscores;
     }
-
     letterArr = currentWord.split("");
     document.getElementById("remaining-guesses").innerHTML = guessLeft;
-    guessedLetters = "";
+    guessedLetters = [];
 }
 
-// Math.floor() rounds to the nearest integer
-// Math.random() chooses a random number between 0 and 1. If I want to,
-// specify a range, then I multiply Math.random() by a range I want to
-// set- in this case the range is the length of my word
-console.log(currentWord);
-
-// this function makes letter blanks
-for (let i = 0; i < currentWord.length; i++) {
-    underscores = underscores + "_";
-    underscoresArr = underscores.split("");
-    console.log(underscoresArr);
-    displayWord.textContent = underscores;
-}
-
-
-// This function handles key events and counts guesses in the "Letters already guessed"
-document.onkeyup = function (event) {
-    let userGuess = event.key;
-    let guessedLetter = userGuess;
-    
-    if (guessedLetters.indexOf(guessedLetter) >= 0) {
-        // this makes sure the user can't guess the same letter more than once
-        alert("You've already guessed '" + userGuess + "'. Try a different letter.")
-    }
-    else {
-        // This logic appends every guess to Letters Already Guessed
-        guessedLetters.push(guessedLetter);
-        console.log(guessedLetters);
-        console.log(typeof(guessedLetters));
-        displayLetter.textContent = guessedLetters;
-        guessLeft--;
-    }
-
-    // if (guessedLetters.includes(userGuess)) {
-    //     // the line below guessedLetters holds each of the user's guesses
-    //     guessedLetters = guessedLetters;
-    //     displayLetter.textContent = guessedLetters;
-    //     alert("You've already guessed '" + userGuess + "'. Try a different letter.")
-    // }
-    // else {
-    //     guessedLetters = guessedLetters + userGuess;
-        
-    //     displayLetter.textContent = guessedLetters;
-    //     guessLeft--;
-    // }
-
-    
-    // the line below displays each of the users guesses
-    displayLetter.textContent = guessedLetters;
-    
-    console.log("guessedLetter: " + guessedLetter);
-    console.log("guessedLetters" + " " + guessedLetters);
-
-    // below updates the guesses left to the browser
-    document.getElementById("remaining-guesses").innerHTML = guessLeft;
-    
-
-    if (guessLeft > 0) {
-        
-        for (let i = 0; i < letterArr.length; i++) {
-            
-            if (guessedLetter === letterArr[i]) {
-                console.log(guessedLetter);
-                underscoresArr.splice(i, 1, guessedLetter);
-
-                console.log(underscoresArr);
-                displayWord.textContent = underscoresArr.join(" ");
-            }
-        }
-    } else {
-
-        document.getElementById("remaining-guesses").innerHTML = guessLeft;
-        wins++;
-        winsDisplay;
-        alert("GAME OVER!");
-        reset();
-    }
-    if (underscoresArr.includes("_") === false) {
-        wins++;
-        winsDisplay.textContent = "Wins: " + wins;
-        setTimeout(function () {
-            alert("You Won!");
-        })
-        reset();
-    }
-}
 // function alreadyGuessed(guess) {
 //     letterGuess.textContent 
 // }
