0b0b8057027cb353d5e4a6c6c785a16f89743f71
Michael Williams <mwilliams2964@gmail.com>
2019-09-03 17:52:03 -0700

    added pictures and sounds and squished bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,8 @@ let displayLettersGuessed = document.getElementById("already-guessed");
 let winsDisplay = document.getElementById("wins")
 let displayWord = document.getElementById("current-word");
 let imageTag = document.createElement("img");
-
+let audioTag = document.createElement("source");
+console.log(currentWord);
 let underscores = "";
 let underscoresArr = [];
 let guessedLetter = "";
@@ -16,6 +17,13 @@ let guessedLetters = [];
 let wins = 0;
 let guessLeft = 13;
 winsDisplay.textContent = "Wins: " + wins;
+let proton = new Audio('./assets/images/proton.wav');
+let velocity = new Audio("./assets/images/velocity.wav");
+let mass = new Audio("./assets/images/mass.wav")
+let energy = new Audio("./assets/images/energy.wav")
+let atom = new Audio("./assets/images/atom.wav")
+let compound = new Audio("./assets/images/compound.mp3")
+let science = new Audio("./assets/images/science.wav")
 
 // this loop makes letter blanks
 for (let i = 0; i < currentWord.length; i++) {
@@ -28,6 +36,7 @@ for (let i = 0; i < currentWord.length; i++) {
 document.onkeyup = function (event) {
     let userGuess = event.key;
     let guessedLetter = userGuess;
+    console.log(underscoresArr.includes("_"))
 
     // This checks to see if they key pressed is a letter of the alphabet
     if (alphabetArr.indexOf(guessedLetter) >= 0) {
@@ -36,68 +45,198 @@ document.onkeyup = function (event) {
         if (guessedLetters.indexOf(guessedLetter) >= 0) {
             alert("You've already guessed '" + userGuess + "'. Try a different letter.")
         }
-        // This logic appends every guess to alreadyguessedarr and updates remaining guesses
+
         else {
-            guessedLetters.push(guessedLetter);
-            displayLettersGuessed.textContent = guessedLetters;
             guessLeft--;
             document.getElementById("remaining-guesses").innerHTML = guessLeft;
-        }
-
-        // This takes user input and puts character in correct position
-        if (guessLeft > 0) {
+            // this fills in the blanks when a user guesses a correct letter
             for (let i = 0; i < lettersOfWordArr.length; i++) {
+                // if the user guesses a correct letter 
                 if (guessedLetter === lettersOfWordArr[i]) {
                     underscoresArr.splice(i, 1, guessedLetter);
                     displayWord.textContent = underscoresArr.join(" ");
                 }
             }
-            // This displays game over and updates values accordingly
-        } else {
-            document.getElementById("remaining-guesses").innerHTML = guessLeft;
-            wins++;
-            guessLeft--;
-            winsDisplay;
-            alert('GAME OVER! The word was "' + currentWord + '."');
-            reset();
+            if (guessLeft === 0 && underscoresArr.includes("_") === false) {
+                wins++;
+                winsDisplay.textContent = "Wins: " + wins;
+                alert("You Won!");
+
+                if (currentWord === wordArr[0]) {
+                    imageTag.setAttribute("src", "./assets/images/proton.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    proton.play();
+                    reset();
+                }
+                else if (currentWord === wordArr[1]) {
+                    imageTag.setAttribute("src", "./assets/images/velocity.jpg")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    velocity.play();
+                    reset();
+                } else if (currentWord === wordArr[2]) {
+                    imageTag.setAttribute("src", "./assets/images/mass.jpg")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    mass.play();
+                    reset();
+                } else if (currentWord === wordArr[3]) {
+                    imageTag.setAttribute("src", "./assets/images/energy.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    energy.play();
+                    reset();
+                } else if (currentWord === wordArr[4]) {
+                    imageTag.setAttribute("src", "./assets/images/atom.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    atom.play();
+                    reset();
+                } else if (currentWord === wordArr[5]) {
+                    imageTag.setAttribute("src", "./assets/images/compound.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    compound.play();
+                    reset();
+                } else if (currentWord === wordArr[6]) {
+                    imageTag.setAttribute("src", "./assets/images/momentum.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    velocity.play();
+                    reset();
+                } else if (currentWord === wordArr[7]) {
+                    imageTag.setAttribute("src", "./assets/images/Science.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    science.play();
+                    reset();
+                }
+                
+                reset();
+            }
+            
+            else if (guessLeft === 0) {
+                
+                alert('GAME OVER! The word was "' + currentWord + '."');
+                reset();
+            }
+
+
+            else if (underscoresArr.includes("_")) {
+                guessedLetters.push(guessedLetter);
+                displayLettersGuessed.textContent = guessedLetters;
+                document.getElementById("remaining-guesses").innerHTML = guessLeft;
+            }
+            else if (underscoresArr.includes("_") === false) {
+                wins++;
+                winsDisplay.textContent = "Wins: " + wins;
+                alert("You Won!");
+
+                if (currentWord === wordArr[0]) {
+                    imageTag.setAttribute("src", "./assets/images/proton.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    proton.play();
+                    reset();
+                }
+                else if (currentWord === wordArr[1]) {
+                    imageTag.setAttribute("src", "./assets/images/velocity.jpg")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    velocity.play();
+                    reset();
+                } else if (currentWord === wordArr[2]) {
+                    imageTag.setAttribute("src", "./assets/images/mass.jpg")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    mass.play();
+                    reset();
+                } else if (currentWord === wordArr[3]) {
+                    imageTag.setAttribute("src", "./assets/images/energy.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    energy.play();
+                    reset();
+                } else if (currentWord === wordArr[4]) {
+                    imageTag.setAttribute("src", "./assets/images/atom.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    atom.play();
+                    reset();
+                } else if (currentWord === wordArr[5]) {
+                    imageTag.setAttribute("src", "./assets/images/compound.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    compound.play();
+                    reset();
+                } else if (currentWord === wordArr[6]) {
+                    imageTag.setAttribute("src", "./assets/images/momentum.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    velocity.play();
+                    reset();
+                } else if (currentWord === wordArr[7]) {
+                    imageTag.setAttribute("src", "./assets/images/Science.png")
+                    document.getElementById("imageDiv").appendChild(imageTag);
+                    science.play();
+                    reset();
+                }
+                // setTimeout(function () {
+                //     alert("You Won!");
+                // })
+                reset();
+            }
+
+
+
+
         }
     }
-    // This displays that user won and then resets the game
-    if (underscoresArr.includes("_") === false) {
-        wins++;
-        winsDisplay.textContent = "Wins: " + wins;
-        if (currentWord === wordArr[0]) {
-            imageTag.setAttribute("src", "./assets/images/proton.png")
-            document.getElementById("imageDiv").appendChild(imageTag);
-        }
-        else if (currentWord === wordArr[1]) {
-            imageTag.setAttribute("src", "./assets/images/velocity.jpg")
-            document.getElementById("imageDiv").appendChild(imageTag);
-        }else if (currentWord === wordArr[2]) {
-            imageTag.setAttribute("src", "./assets/images/mass.jpg")
-            document.getElementById("imageDiv").appendChild(imageTag);
-        }else if (currentWord === wordArr[3]) {
-            imageTag.setAttribute("src", "./assets/images/energy.png")
-            document.getElementById("imageDiv").appendChild(imageTag);
-        }else if (currentWord === wordArr[4]) {
-            imageTag.setAttribute("src", "./assets/images/atom.png")
-            document.getElementById("imageDiv").appendChild(imageTag);
-        }else if (currentWord === wordArr[5]) {
-            imageTag.setAttribute("src", "./assets/images/compound.png")
-            document.getElementById("imageDiv").appendChild(imageTag);
-        }else if (currentWord === wordArr[6]) {
-            imageTag.setAttribute("src", "./assets/images/momentum.png")
-            document.getElementById("imageDiv").appendChild(imageTag);
-        }else if (currentWord === wordArr[7]) {
-            imageTag.setAttribute("src", "./assets/images/Science.png")
-            document.getElementById("imageDiv").appendChild(imageTag);
-        }
-        setTimeout(function () {
-            alert("You Won!");
-        })
-        reset();
-    }
+
+
+    // else if (underscoresArr.includes("_") === false) {
+    //     wins++;
+    //     winsDisplay.textContent = "Wins: " + wins;
+    //     alert("You Won!");
+    //     if (currentWord === wordArr[0]) {
+    //         imageTag.setAttribute("src", "./assets/images/proton.png")
+    //         document.getElementById("imageDiv").appendChild(imageTag);
+    //     }
+    //     else if (currentWord === wordArr[1]) {
+    //         imageTag.setAttribute("src", "./assets/images/velocity.jpg")
+    //         document.getElementById("imageDiv").appendChild(imageTag);
+    //     } else if (currentWord === wordArr[2]) {
+    //         imageTag.setAttribute("src", "./assets/images/mass.jpg")
+    //         document.getElementById("imageDiv").appendChild(imageTag);
+    //     } else if (currentWord === wordArr[3]) {
+    //         imageTag.setAttribute("src", "./assets/images/energy.png")
+    //         document.getElementById("imageDiv").appendChild(imageTag);
+    //     } else if (currentWord === wordArr[4]) {
+    //         imageTag.setAttribute("src", "./assets/images/atom.png")
+    //         document.getElementById("imageDiv").appendChild(imageTag);
+    //     } else if (currentWord === wordArr[5]) {
+    //         imageTag.setAttribute("src", "./assets/images/compound.png")
+    //         document.getElementById("imageDiv").appendChild(imageTag);
+    //     } else if (currentWord === wordArr[6]) {
+    //         imageTag.setAttribute("src", "./assets/images/momentum.png")
+    //         document.getElementById("imageDiv").appendChild(imageTag);
+    //     } else if (currentWord === wordArr[7]) {
+    //         imageTag.setAttribute("src", "./assets/images/Science.png")
+    //         document.getElementById("imageDiv").appendChild(imageTag);
+    //     }
+    //     // setTimeout(function () {
+    //     //     alert("You Won!");
+    //     // })
+    //     reset();
+    // }
+
+
 }
+
+// if there are no more guesses left and if there are no underscores left in array
+
+// if there are no underscores left
+
+
+
+
+// This logic appends every guess to alreadyguessedarr and updates remaining guesses
+// This takes user input and puts character in correct position
+
+// This displays game over and updates values accordingly
+
+
+
+
+// This displays that user won and then resets the game
+
+
 
 // This resets all values for a brand new word
 function reset() {
